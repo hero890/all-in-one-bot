cd87c789dd9404d8c425521bf1a5d8f5d558ade3
hero890 <159156837+hero890@users.noreply.github.com>
2024-03-09 17:05:21 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,8 +1,8 @@
 # Bot information
 SESSION = 'Media_search'
-USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
+USER_SESSION = '@forward_botss_bot'
+API_ID = '22672907'
+API_HASH = 'https://github.com/GamingBeast1/Anaya-bot-of-bh-botz.git'
 BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
 USERBOT_STRING_SESSION = ''
 
@@ -11,13 +11,13 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
+ADMINS = [6287591671, 'admin123', 6287591671]
 CHANNELS = [-10012345678, -100987654321, 'channelusername']
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://rani828719:sVyRWZOrUzIWNfHp@cluster0.zodktob.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
